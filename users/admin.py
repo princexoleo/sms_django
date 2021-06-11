cd494edaf10efc1cda9ea433e2cbacc79029a8b4
princexoleo <xtleon.xl@gmail.com>
2021-06-11 22:41:31 +0600

    Modifies the models and added the student, subject model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import SubjectsModel, StudentsModel
 
 # Register your models here.
+@admin.register(SubjectsModel)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject_name', 'is_optional']
+    list_filter = ['is_optional']
+    search_fields = ['id', 'subject_name']
+
+
+@admin.register(StudentsModel)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'student_name', 'student_roll', 'department_name',
+                    'batch_session', 'created_user',
+                    'subject_1', 'subject_2', 'subject_3', 'subject_4',
+                    'subject_5', 'subject_6', 'subject_optional']
+    list_filter = ['department_name', 'batch_session', 'created_user']
+    search_fields = ['id', 'student_name', 'student_roll']
+
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import SubjectsModel, StudentsModel
  
  
  
@@ -12,3 +13,41 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'phone_no', 'password1', 'password2']
+
+
+
+class SubjectsForm(forms.Form):
+    ubject_name = forms.CharField(
+        label='',
+        max_length=30,
+        min_length=5,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Subject Name",
+                "class": "form-control"
+            }
+        )
+    )
+    is_optional = forms.BooleanField(
+        required=False,
+        label="Check the box if it is optional subjects",
+        widget= forms.CheckboxInput(
+            attrs={
+                "class": "form-control"
+            })
+    )
+
+    class Meta:
+        model = SubjectsModel
+        fields = ['subject_name','is_optional']
+
+
+## StudentModels forms
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = StudentsModel
+        fields = ['student_name', 'student_roll','batch_session','department_name',
+                  'subject_1', 'subject_2', 'subject_3', 'subject_4', 'subject_5',
+                  'subject_6', 'subject_optional', 'created_user']
